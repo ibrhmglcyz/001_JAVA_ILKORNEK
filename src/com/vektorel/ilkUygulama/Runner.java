4e017403c89295c06c8fe3159b78a2274044956e
ibrhmglcyz <ibrhmglcyz@192.168.1.21>
2019-05-21 17:30:49 +0300

    Java ilk ornek uygulamasi. 06.04.2019

---
+++ src/com/vektorel/ilkUygulama/Runner.java
@@ -0,0 +1,16 @@
+package com.vektorel.ilkUygulama;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Muhammet");
+		System.out.println("Muhammet2");
+		System.out.print("Muhammet3 - ");
+		System.out.println("SOYAD");
+
+		System.err.println("HATAAAAAAA");
+
+	}
+
+}
